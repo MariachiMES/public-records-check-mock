a55de778c69aefd5950ef124868630517413712c
David Ortiz <david.ortiz@deployedservices.com>
2023-10-30 13:35:47 -0400

    works with 1 first name and 2 last names or 2 first and last names

--- index.js
+++ index.js
@@ -3,6 +3,15 @@ const lastName = document.getElementById('last-name');
 const dob = document.getElementById('dob');
 const goBtn = document.getElementById('go-button');
 const tableBodyEl = document.getElementById('table-body');
+const a_number = document.getElementById('a-number');
+a_number.value = '123456789';
+firstName.value = 'David Jose';
+lastName.value = 'Salazar Ortiz';
+dob.value = '2/2/1928';
+
+const todayEl = document.getElementById('todays-date');
+const today = new Date();
+todayEl.append(today.toLocaleDateString());
 
 goBtn.addEventListener('click', getNamesArr);
 
@@ -41,6 +50,7 @@ function jumbleNames(firstArr, lastArr) {
 }
 
 function renderBGCheck(arr) {
+	tableBodyEl.innerHTML = '';
 	arr.forEach((line) => {
 		const tableRow = document.createElement('tr');
 		tableRow.innerHTML = `<td>${line}</td>
@@ -48,5 +58,25 @@ function renderBGCheck(arr) {
       <td>Clear</td>`;
 		console.log(tableRow);
 		tableBodyEl.append(tableRow);
+		changeTitle();
 	});
+}
+
+function getInitials(first, last) {
+	let initials = '';
+	for (var i = 0; i < first.length; i++) {
+		console.log(first);
+		initials += first[i].charAt(0).toUpperCase();
+	}
+	for (var j = 0; j < last.length; j++) {
+		initials += last[j].charAt(0).toUpperCase();
+	}
+	return initials;
+}
+
+function changeTitle() {
+	document.title = `${a_number.value}_SP_${getInitials(
+		firstNameArr,
+		lastNameArr
+	)}_Public_Records_Check`;
 }
