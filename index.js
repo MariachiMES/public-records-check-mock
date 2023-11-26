ed4807145bb2ad7351258ba65991660c29d0551b
David Ortiz <david.ortiz@deployedservices.com>
2023-11-26 11:56:43 -0500

    proof of address bugs fixed

--- index.js
+++ index.js
@@ -19,6 +19,21 @@ const childDob = document.getElementById('child-dob');
 const childName = document.getElementById('child-name');
 const fingerprintConfirmBtn = document.getElementById('fingerprint-confirm');
 
+firstName.value = 'David Jose';
+lastName.value = 'Ortiz Salazar';
+dob.value = '2/26/1983';
+a_number.value = '123123123';
+stateEl.value = 'TX';
+relationship.value = 'Father';
+caseManager.value = 'Hector Barberi';
+phoneNum.value = 2342342345;
+zipCode.value = 24534;
+city.value = 'Crystal City';
+address.value = '203 N Ave C';
+results.value = 'Clear';
+childDob.value = '6/3/2017';
+childName.value = 'Lorenzo Ortiz';
+
 goBtn.addEventListener('click', getNamesArr);
 fingerPrintBtn.addEventListener('click', renderFingerprints);
 lopcBtn.addEventListener('click', renderLOPC);
@@ -207,28 +222,6 @@ function colorHandler(element) {
 	element.classList.remove('missing');
 }
 
-function renderPoa() {
-	colorsBackToNormal();
-	const poaNodes = document.querySelectorAll('.poa');
-	let poaData = [];
-	poaNodes.forEach((item) => {
-		if (item.value === '') {
-			console.log(item.classList);
-			errorColorHandling(item);
-		} else {
-			poaData.push(item.value);
-			colorHandler(item);
-		}
-	});
-	if (poaData.length < 6) {
-		return alert(
-			`Proof of address requires the child's A-Number, Sponsor's first name, last name, address, city, state, zip code`
-		);
-	}
-	pageBodyEl.innerHTML = proofOfAddressTemplate;
-	changeTitle('Proof_Address');
-}
-
 let firstNameArr = [];
 let lastNameArr = [];
 
@@ -382,224 +375,244 @@ function colorsBackToNormal() {
 	});
 }
 
-let proofOfAddressTemplate = `<style>
-body{
-	margin-top:20px;
-
+function renderPoa() {
+	colorsBackToNormal();
+	const poaNodes = document.querySelectorAll('.poa');
+	let poaData = [];
+	poaNodes.forEach((item) => {
+		if (item.value === '') {
+			console.log(item.classList);
+			errorColorHandling(item);
+		} else {
+			poaData.push(item.value);
+			colorHandler(item);
+		}
+	});
+	if (poaData.length < 8) {
+		return alert(
+			`Proof of address requires the child's A-Number, Sponsor's first name, last name, address, city, state, zip code`
+		);
+	}
+	pageBodyEl.innerHTML = `<style>
+	body{
+		margin-top:20px;
+	
+	}
+	
+	.invoice {
+		background: #fff;
+		padding: 20px
+	}
+	
+	.invoice-company {
+		font-size: 20px
+	}
+	
+	.invoice-header {
+		margin: 0 -20px;
+		background: #f0f3f4;
+		padding: 20px
+	}
+	
+	.invoice-date,
+	.invoice-from,
+	.invoice-to {
+		display: table-cell;
+		width: 1%
+	}
+	
+	.invoice-from,
+	.invoice-to {
+		padding-right: 20px
+	}
+	
+	.invoice-date .date,
+	.invoice-from strong,
+	.invoice-to strong {
+		font-size: 16px;
+		font-weight: 600
+	}
+	
+	.invoice-date {
+		text-align: right;
+		padding-left: 20px
+	}
+	
+	.invoice-price {
+		background: #f0f3f4;
+		display: table;
+		width: 100%
+	}
+	
+	.invoice-price .invoice-price-left,
+	.invoice-price .invoice-price-right {
+		display: table-cell;
+		padding: 20px;
+		font-size: 20px;
+		font-weight: 600;
+		width: 75%;
+		position: relative;
+		vertical-align: middle
+	}
+	
+	.invoice-price .invoice-price-left .sub-price {
+		display: table-cell;
+		vertical-align: middle;
+		padding: 0 20px
+	}
+	
+	.invoice-price small {
+		font-size: 12px;
+		font-weight: 400;
+		display: block
+	}
+	
+	.invoice-price .invoice-price-row {
+		display: table;
+		float: left
+	}
+	
+	.invoice-price .invoice-price-right {
+		width: 25%;
+		background: #2d353c;
+		color: #fff;
+		font-size: 28px;
+		text-align: right;
+		vertical-align: bottom;
+		font-weight: 300
+	}
+	
+	.invoice-price .invoice-price-right small {
+		display: block;
+		opacity: .6;
+		position: absolute;
+		top: 10px;
+		left: 10px;
+		font-size: 12px
+	}
+	
+	.invoice-footer {
+		border-top: 1px solid #ddd;
+		padding-top: 10px;
+		font-size: 10px
+	}
+	
+	.invoice-note {
+		color: #999;
+		margin-top: 80px;
+		font-size: 85%
+	}
+	
+	.invoice>div:not(.invoice-footer) {
+		margin-bottom: 20px
+	}
+	
+	.btn.btn-white, .btn.btn-white.disabled, .btn.btn-white.disabled:focus, .btn.btn-white.disabled:hover, .btn.btn-white[disabled], .btn.btn-white[disabled]:focus, .btn.btn-white[disabled]:hover {
+		color: #2d353c;
+		background: #fff;
+		border-color: #d9dfe3;
+	}
+	</style>
+	<div class="container">
+	<div class="col-md-12">
+	  <div class="invoice">
+		 <!-- begin invoice-company -->
+		 <div class="invoice-company text-inverse f-w-600">
+			DS Utility, Inc (TRAINING)
+		 </div>
+		 <!-- end invoice-company -->
+		 <!-- begin invoice-header -->
+		 <div class="invoice-header">
+			<div class="invoice-from">
+			   <address class="m-t-5 m-b-5">
+				  <strong class="text-inverse">DS Utility, Inc.</strong><br>
+				  1234 Main Street<br>
+				  ${city.value}, ${stateEl.value} ${zipCode.value}<br>
+				  Phone: (123) 000-0000<br>
+				  Fax: (123) 000-0001
+			   </address>
+			</div>
+			<div class="invoice-to">
+			   <address class="m-t-5 m-b-5">
+				  <strong class="text-inverse">${firstName.value} ${lastName.value}</strong><br>
+				  ${address.value}<br>
+				  ${city.value}, ${stateEl.value} ${zipCode.value}<br>
+				  Phone: ${parsePhoneNum(phoneNum.value)}<br>
+			   </address>
+			</div>
+			<div class="invoice-date">
+			   <small>Invoice / ${billMonth} period</small>
+			   <div class="date text-inverse m-t-5">${fullBillDate}</div>
+			   <div class="invoice-detail">
+				  #0000123DSS<br>
+				  Electric Bill
+			   </div>
+			</div>
+		 </div>
+		 <!-- end invoice-header -->
+		 <!-- begin invoice-content -->
+		 <div class="invoice-content">
+			<!-- begin table-responsive -->
+			<div class="table-responsive">
+			   <table class="table table-invoice">
+				  <thead>
+					 <tr>
+						<th>DESCRIPTION</th>
+						<th class="text-center" width="10%">WATER</th>
+						<th class="text-center" width="10%">WASTE</th>
+						<th class="text-right" width="20%">ELECTRICITY</th>
+					 </tr>
+				  </thead>
+				  <tbody>
+					 <tr>
+						<td>
+						   <span class="text-inverse">Water, Electricity, Waste</span><br>
+						   <small>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed id sagittis arcu.</small>
+						</td>
+						<td class="text-center">$50.00</td>
+						<td class="text-center">$20.00</td>
+						<td class="text-right">$90.00</td>
+					 </tr>
+					 
+				  </tbody>
+			   </table>
+			</div>
+			<!-- end table-responsive -->
+			<!-- begin invoice-price -->
+			<div class="invoice-price">
+			   <div class="invoice-price-left">
+				  <div class="invoice-price-row">
+					
+				  </div>
+			   </div>
+			   <div class="invoice-price-right">
+				  <small>TOTAL</small> <span class="f-w-600">$160.00</span>
+			   </div>
+			</div>
+			<!-- end invoice-price -->
+		 </div>
+		 <!-- end invoice-content -->
+		 <!-- begin invoice-note -->
+		 <div class="invoice-note">
+			* Make all checks payable to DS Utility<br>
+			* Payment is due within 30 days<br>
+			* If you have any questions concerning this invoice, contact AC Slater at T: 123.555.0000 E: BadBunny@greensborolive.com 
+		 </div>
+		 <!-- end invoice-note -->
+		 <!-- begin invoice-footer -->
+		 <div class="invoice-footer">
+			<p class="text-center m-b-5 f-w-600">
+			   THANK YOU FOR YOUR BUSINESS
+			</p>
+			<p class="text-center">
+			   <span class="m-r-10"><i class="fa fa-fw fa-lg fa-globe"></i> dsutility.com</span>
+			   <span class="m-r-10"><i class="fa fa-fw fa-lg fa-phone-volume"></i> T: 123-000-0000</span>
+			   <span class="m-r-10"><i class="fa fa-fw fa-lg fa-envelope"></i> training@dsutility.com</span>
+			</p>
+		 </div>
+		 <!-- end invoice-footer -->
+	  </div>
+	</div>
+	</div>`;
+	changeTitle('Proof_Address');
 }
-
-.invoice {
-	background: #fff;
-	padding: 20px
-}
-
-.invoice-company {
-	font-size: 20px
-}
-
-.invoice-header {
-	margin: 0 -20px;
-	background: #f0f3f4;
-	padding: 20px
-}
-
-.invoice-date,
-.invoice-from,
-.invoice-to {
-	display: table-cell;
-	width: 1%
-}
-
-.invoice-from,
-.invoice-to {
-	padding-right: 20px
-}
-
-.invoice-date .date,
-.invoice-from strong,
-.invoice-to strong {
-	font-size: 16px;
-	font-weight: 600
-}
-
-.invoice-date {
-	text-align: right;
-	padding-left: 20px
-}
-
-.invoice-price {
-	background: #f0f3f4;
-	display: table;
-	width: 100%
-}
-
-.invoice-price .invoice-price-left,
-.invoice-price .invoice-price-right {
-	display: table-cell;
-	padding: 20px;
-	font-size: 20px;
-	font-weight: 600;
-	width: 75%;
-	position: relative;
-	vertical-align: middle
-}
-
-.invoice-price .invoice-price-left .sub-price {
-	display: table-cell;
-	vertical-align: middle;
-	padding: 0 20px
-}
-
-.invoice-price small {
-	font-size: 12px;
-	font-weight: 400;
-	display: block
-}
-
-.invoice-price .invoice-price-row {
-	display: table;
-	float: left
-}
-
-.invoice-price .invoice-price-right {
-	width: 25%;
-	background: #2d353c;
-	color: #fff;
-	font-size: 28px;
-	text-align: right;
-	vertical-align: bottom;
-	font-weight: 300
-}
-
-.invoice-price .invoice-price-right small {
-	display: block;
-	opacity: .6;
-	position: absolute;
-	top: 10px;
-	left: 10px;
-	font-size: 12px
-}
-
-.invoice-footer {
-	border-top: 1px solid #ddd;
-	padding-top: 10px;
-	font-size: 10px
-}
-
-.invoice-note {
-	color: #999;
-	margin-top: 80px;
-	font-size: 85%
-}
-
-.invoice>div:not(.invoice-footer) {
-	margin-bottom: 20px
-}
-
-.btn.btn-white, .btn.btn-white.disabled, .btn.btn-white.disabled:focus, .btn.btn-white.disabled:hover, .btn.btn-white[disabled], .btn.btn-white[disabled]:focus, .btn.btn-white[disabled]:hover {
-	color: #2d353c;
-	background: #fff;
-	border-color: #d9dfe3;
-}
-</style>
-<div class="container">
-<div class="col-md-12">
-  <div class="invoice">
-	 <!-- begin invoice-company -->
-	 <div class="invoice-company text-inverse f-w-600">
-		DS Utility, Inc
-	 </div>
-	 <!-- end invoice-company -->
-	 <!-- begin invoice-header -->
-	 <div class="invoice-header">
-		<div class="invoice-from">
-		   <address class="m-t-5 m-b-5">
-			  <strong class="text-inverse">DS Utility, Inc.</strong><br>
-			  1234 Main Street<br>
-			  ${city.value}, ${stateEl.value} ${zipCode.value}<br>
-			  Phone: (123) 000-0000<br>
-			  Fax: (123) 000-0001
-		   </address>
-		</div>
-		<div class="invoice-to">
-		   <address class="m-t-5 m-b-5">
-			  <strong class="text-inverse">${firstName.value} ${lastName.value}</strong><br>
-			  ${address.value}<br>
-			  ${city.value}, ${stateEl.value} ${zipCode.value}<br>
-			  Phone: ${parsePhoneNum(phoneNum.value)}]<br>
-		   </address>
-		</div>
-		<div class="invoice-date">
-		   <small>Invoice / ${billMonth} period</small>
-		   <div class="date text-inverse m-t-5">${fullBillDate}</div>
-		   <div class="invoice-detail">
-			  #0000123DSS<br>
-			  Electric Bill
-		   </div>
-		</div>
-	 </div>
-	 <!-- end invoice-header -->
-	 <!-- begin invoice-content -->
-	 <div class="invoice-content">
-		<!-- begin table-responsive -->
-		<div class="table-responsive">
-		   <table class="table table-invoice">
-			  <thead>
-				 <tr>
-					<th>DESCRIPTION</th>
-					<th class="text-center" width="10%">WATER</th>
-					<th class="text-center" width="10%">WASTE</th>
-					<th class="text-right" width="20%">ELECTRICITY</th>
-				 </tr>
-			  </thead>
-			  <tbody>
-				 <tr>
-					<td>
-					   <span class="text-inverse">Water, Electricity, Waste</span><br>
-					   <small>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed id sagittis arcu.</small>
-					</td>
-					<td class="text-center">$50.00</td>
-					<td class="text-center">$20.00</td>
-					<td class="text-right">$90.00</td>
-				 </tr>
-				 
-			  </tbody>
-		   </table>
-		</div>
-		<!-- end table-responsive -->
-		<!-- begin invoice-price -->
-		<div class="invoice-price">
-		   <div class="invoice-price-left">
-			  <div class="invoice-price-row">
-				
-			  </div>
-		   </div>
-		   <div class="invoice-price-right">
-			  <small>TOTAL</small> <span class="f-w-600">$160.00</span>
-		   </div>
-		</div>
-		<!-- end invoice-price -->
-	 </div>
-	 <!-- end invoice-content -->
-	 <!-- begin invoice-note -->
-	 <div class="invoice-note">
-		* Make all checks payable to DS Utility<br>
-		* Payment is due within 30 days<br>
-		* If you have any questions concerning this invoice, contact AC Slater at T: 123.555.0000 E: BadBunny@greensborolive.com 
-	 </div>
-	 <!-- end invoice-note -->
-	 <!-- begin invoice-footer -->
-	 <div class="invoice-footer">
-		<p class="text-center m-b-5 f-w-600">
-		   THANK YOU FOR YOUR BUSINESS
-		</p>
-		<p class="text-center">
-		   <span class="m-r-10"><i class="fa fa-fw fa-lg fa-globe"></i> dsutility.com</span>
-		   <span class="m-r-10"><i class="fa fa-fw fa-lg fa-phone-volume"></i> T: 123-000-0000</span>
-		   <span class="m-r-10"><i class="fa fa-fw fa-lg fa-envelope"></i> training@dsutility.com</span>
-		</p>
-	 </div>
-	 <!-- end invoice-footer -->
-  </div>
-</div>
-</div>`;
